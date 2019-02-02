9d18f5acb677ec5c45a0a88b162e9dd870c1ed96
Mike <mike@mikejarosch.com>
2019-02-02 14:25:58 -0600

    Initial commit

---
+++ StopWatch/ViewController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace StopWatch
+{
+    public partial class ViewController : UIViewController
+    {
+        int _time;
+        NSTimer _timer;
+
+        partial void Reset_UpInside(UIButton sender)
+        {
+            _time = 0;
+            Reset.Enabled = false;
+            UpdateLabel();
+        }
+
+        partial void StartStop_UpInside(UIButton sender)
+        {
+            Reset.Enabled = true;
+
+            if (_timer == null)
+            {
+                StartStop.SetTitle("Stop", UIControlState.Normal);
+                _timer = NSTimer.CreateRepeatingScheduledTimer(0.01, Timer_Tick);
+            }
+            else
+            {
+                StartStop.SetTitle("Start", UIControlState.Normal);
+                _timer.Invalidate();
+                _timer = null;
+            }
+        }
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        private void Timer_Tick(NSTimer timer)
+        {
+            _time++;
+            UpdateLabel();
+        }
+
+        private void UpdateLabel()
+        {
+            var hundredth = _time % 100;
+            var seconds = (_time / 100) % 60;
+            var minutes = (_time / 6000) % 60;
+            var hours = _time / 360000;
+            timerDisplay.Text = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", hours, minutes, seconds, hundredth);
+        }
+    }
+}
---
+++ StopWatch/ViewController.designer.cs
@@ -0,0 +1,54 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace StopWatch
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Reset { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton StartStop { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel timerDisplay { get; set; }
+
+        [Action ("Reset_UpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Reset_UpInside (UIKit.UIButton sender);
+
+        [Action ("StartStop_UpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void StartStop_UpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Reset != null) {
+                Reset.Dispose ();
+                Reset = null;
+            }
+
+            if (StartStop != null) {
+                StartStop.Dispose ();
+                StartStop = null;
+            }
+
+            if (timerDisplay != null) {
+                timerDisplay.Dispose ();
+                timerDisplay = null;
+            }
+        }
+    }
+}
